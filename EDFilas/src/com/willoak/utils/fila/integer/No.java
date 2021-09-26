fc539dfa55423c09e53f08f1e190a046dd9cab3f
will-oak <88799329+will-oak@users.noreply.github.com>
2021-09-26 16:27:55 -0300

    Add files via upload

---
+++ EDFilas/src/com/willoak/utils/fila/integer/No.java
@@ -0,0 +1,13 @@
+package com.willoak.utils.fila.integer;
+
+public class No {
+
+	int dado;
+	No proximo;
+	
+	@Override
+	public String toString() {
+		return "Dado: "+dado;
+	}
+	
+}
---
+++ EDFilas/src/com/willoak/utils/fila/string/No.java
@@ -0,0 +1,13 @@
+package com.willoak.utils.fila.string;
+
+public class No {
+
+	String dado;
+	No proximo;
+	
+	@Override
+	public String toString() {
+		return "Dado: " + dado;
+	}
+	
+}
